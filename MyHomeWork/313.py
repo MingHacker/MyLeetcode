4b88a729ccb82fcc65cdb913bb68155abaf3cb19
minghacker <maming1990@gmail.com>
2021-11-27 15:28:40 -0700

    313

---
+++ MyHomeWork/313.py
@@ -0,0 +1,32 @@
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        ans = [1]
+        k = len(primes)
+        pointers = [0] * k
+        for _ in range(1, n):
+            # find the next smallest value:
+            options = []
+            for j in range(k):
+                options.append(ans[pointers[j]] * primes[j])
+            selected = min(options)
+            ans.append(selected)
+            for j in range(k):
+                if options[j] == selected:
+                    pointers[j] += 1
+        return ans[-1]
+
+
+# heap
+
+# to get the nth smallest ugly number, need to use a minHeap
+# keep popping out n times
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        h = [1]
+        for _ in range(n):
+            curr = heapq.heappop(h)
+            while h and h[0] == curr:
+                curr = heapq.heappop(h)
+            for prime in primes:
+                heapq.heappush(h, prime * curr)
+        return curr
