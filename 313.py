7ff34cd1b81d7278db4b21a498c2a7c3fb80c90a
Ming Ma <UA17466@DOW.COM>
2021-12-21 20:56:44 -0700

    update

--- 313.py
+++ 313.py
@@ -30,3 +30,24 @@ class Solution:
             for prime in primes:
                 heapq.heappush(h, prime * curr)
         return curr
+
+# k merge with heapq
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes:List[int]) -> int:
+        h = []
+        for prime in primes:
+            h.append((prime, prime, 1))
+        
+        ugly = [1]
+        pre = 1
+        while n - 1 > 0:
+
+            curr, prime, index = heapq.heappop(h)
+            if pre != curr:
+                ugly.append(curr)
+                n -= 1
+            
+            heapq.heappush(h, (ugly[index] * prime, prime, index))
+
+        return ugly[-1]
+            
